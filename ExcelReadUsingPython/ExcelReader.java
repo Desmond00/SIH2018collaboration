ee39460959c8efa4fc04bd0fbca86d37c5d66c45
Desmond00 <ranjandebnath.rd@gmail.com>
2018-03-13 17:45:44 +0530

    initial commit with 2014 admission dataset in standard format

---
+++ ExcelReadUsingPython/ExcelReader.java
@@ -0,0 +1,58 @@
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class ExcelReader {
+    public static final String SAMPLE_XLSX_FILE_PATH = "./sample-xlsx-file.xlsx";
+
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+
+        // Creating a Workbook from an Excel file (.xls or .xlsx)
+        Workbook workbook = WorkbookFactory.create(new File("data.xlsx"));
+
+        // Retrieving the number of sheets in the Workbook
+        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+        /*
+           =============================================================
+           Iterating over all the sheets in the workbook (Multiple ways)
+           =============================================================
+        */
+
+        // 1. You can obtain a sheetIterator and iterate over it
+
+        // 2. Or you can use a for-each loop
+        System.out.println("Retrieving Sheets using for-each loop");
+        for(Sheet sheet: workbook) {
+            System.out.println("=> " + sheet.getSheetName());
+        }
+
+        /*
+           ==================================================================
+           Iterating over all the rows and columns in a Sheet (Multiple ways)
+           ==================================================================
+        */
+
+        // Getting the Sheet at index zero
+        Sheet sheet = workbook.getSheetAt(0);
+
+        // Create a DataFormatter to format and get each cell's value as String
+        DataFormatter dataFormatter = new DataFormatter();
+
+        
+        // 2. Or you can use a for-each loop to iterate over the rows and columns
+        System.out.println("\n\nIterating over Rows and Columns using for-each loop\n");
+        for (Row row: sheet) {
+            for(Cell cell: row) {
+                String cellValue = dataFormatter.formatCellValue(cell);
+                System.out.print(cellValue + "\t");
+            }
+            System.out.println();
+        }
+
+        // Closing the workbook
+        workbook.close();
+    }
+}
